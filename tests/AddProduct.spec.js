5ec0be1214395e53d7113e5ecb16631b42c30d8e
awareXgit <qaemtest@gmail.com>
2025-02-26 14:41:44 +0700

    update code

--- tests/AddProduct.spec.js
+++ tests/AddProduct.spec.js
@@ -1,7 +1,6 @@
 const { test, expect } = require('@playwright/test');
-const { login } = require('./login'); // ใช้ require() เพื่อนำเข้า login ฟังก์ชัน
+const { login } = require('./login');
 
 test('should log in successfully with correct credentials', async ({ page }) => {
     await login(page, 'standard_user', 'secret_sauce');  // ส่งค่าพารามิเตอร์ username และ password
-    // เพิ่มการตรวจสอบอื่น ๆ ตามที่ต้องการ
 });
--- tests/login.js
+++ tests/login.js
@@ -1,9 +1,9 @@
 const { expect } = require('@playwright/test');
 
-async function login(page, standard_user, secret_sauce) {
+async function login(page, username, password) {
     await page.goto('https://www.saucedemo.com/');
-    await page.locator('#user-name').fill(standard_user);  // ใช้พารามิเตอร์ที่รับเข้ามา
-    await page.locator('#password').fill(secret_sauce);  // ใช้พารามิเตอร์ที่รับเข้ามา
+    await page.locator('#user-name').fill(username);  // ใช้พารามิเตอร์ที่รับเข้ามา
+    await page.locator('#password').fill(password);  // ใช้พารามิเตอร์ที่รับเข้ามา
     await page.locator('#login-button').click(); 
     await expect(page.locator('.inventory_list')).toBeVisible(); // เช็คว่า inventory list ปรากฏ
 }
