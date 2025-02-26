6f483ae45845ed70f48b4deccb2891d72f549cc3
awareXgit <qaemtest@gmail.com>
2025-02-26 13:51:42 +0700

    delete file add product

--- tests/AddProduct.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import './Login.spec.js';
-
-test('Call Test Case 7 from LoginScenario', async () => {
-    test('Login successfully to web', async ({ page }) => {
-      // code for test case 5 ที่เรานำเข้า
-    });
-  });
