4ed98ca1df2b0555bf5ddc3b67a39a71e8f8b2a2
awareXgit <qaemtest@gmail.com>
2025-02-26 13:48:01 +0700

    update test title

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('Login with blank fields', async ({page}) =>{ // Renamed from "Login with blank fill"
+test('Login with blank all fields', async ({page}) =>{ // Renamed from "Login with blank fill"
     await page.goto('https://www.saucedemo.com/')
     await expect(page.locator('#login-button')).toBeVisible();
     await page.locator('#login-button').click(); 
@@ -22,7 +22,7 @@ test('Login with blank password', async ({page}) =>{
     await expect(page.locator('text=Epic sadface: Password is required')).toBeVisible();
 });
 
-test('Login with invalid user name and password', async ({page}) =>{
+test('Login with invalid username and password', async ({page}) =>{
     await page.goto('https://www.saucedemo.com/')
     await page.locator('#user-name').fill('standard');
     await page.locator('#password').fill('secret111');
@@ -32,16 +32,16 @@ test('Login with invalid user name and password', async ({page}) =>{
 
 test('Login with invalid password', async ({page}) =>{
     await page.goto('https://www.saucedemo.com/')
-    await page.locator('#user-name').fill('standard');
+    await page.locator('#user-name').fill('standard_user');
     await page.locator('#password').fill('t111');
     await page.locator('#login-button').click(); 
     await expect(page.locator('text=Epic sadface: Username and password do not match any user in this service')).toBeVisible();
 });
 
-test('Login with valid username but invalid password', async ({page}) =>{
+test('Login with invalid username', async ({page}) =>{
     await page.goto('https://www.saucedemo.com/')
-    await page.locator('#user-name').fill('standard555');
-    await page.locator('#password').fill('secret111');
+    await page.locator('#user-name').fill('standard999');
+    await page.locator('#password').fill('secret_sauce');
     await page.locator('#login-button').click(); 
     await expect(page.locator('text=Epic sadface: Username and password do not match any user in this service')).toBeVisible();
 });
